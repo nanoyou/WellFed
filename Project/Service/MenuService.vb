4d7c64a6f290d48e4e148db36681302dcb690479
AkagawaTsurunaki <965145619@qq.com>
2022-10-11 23:19:44 +0800

    feat: 实现菜单数据的硬编码

--- Project/Service/MenuService.vb
+++ Project/Service/MenuService.vb
@@ -6,41 +6,39 @@ Namespace AkagawaTsurunaki
     Namespace WellFed
         Namespace Service
             Public Class MenuService : Implements Interfaces.PreGenerator
+                Private Shared Property menuTable As Table(Of Entity.MenuItem)
 
                 Public Sub PreGenerate() Implements PreGenerator.PreGenerate
 
+                    Dim fun = Function(ByVal tagName As String, ByRef items As String())
+                                  For Each item In items
+                                      Dim tagId = TagService.Instance.FindTagByName(tagName).First.tagId
+                                      Dim menuItem = New Entity.MenuItem()
+                                      menuItem.Price = New Decimal(Int(Rnd() * 60))
+                                      menuItem.Name = item
+                                      menuItem.TagIds.Add(tagId)
+                                  Next
+                              End Function
+
                     ' 主食
-                    Dim porridge As String() = {"小米粥", "皮蛋瘦肉粥", "八宝粥", "黑米粥", "绿豆粥"}
-
-
-
-
-                    Dim baozi As String() = {"猪肉包", "牛肉包", "素菜包", "豆沙包"}
-                    Dim huntun As String = "馄饨"
-                    Dim pancake As String() = {"葱油饼", "糖饼", "牛肉馅饼", "猪肉馅饼"}
-
-
+                    fun.Invoke("粥", {"小米粥", "皮蛋瘦肉粥", "八宝粥", "黑米粥", "绿豆粥"})
+                    fun.Invoke("包子", {"猪肉包", "牛肉包", "素菜包", "豆沙包"})
+                    fun.Invoke("馄饨", {"馄饨"})
+                    fun.Invoke("饼", {"葱油饼", "糖饼", "牛肉馅饼", "猪肉馅饼"})
 
                     ' 菜品
-                    Dim liangbancai As String = "凉拌菜"
-                    Dim egg As String() = {"白煮蛋", "茶叶蛋", "煎蛋", "咸鸭蛋"}
+                    fun.Invoke("凉拌菜", {"凉拌菜"})
+                    fun.Invoke("蛋", {"白煮蛋", "茶叶蛋", "煎蛋", "咸鸭蛋"})
 
                     ' 饮品
-                    Dim doujiang As String() = {"热豆浆", "凉豆浆"}
-                    Dim water As String = "矿泉水"
-                    Dim milk As String() = {"热牛奶", "凉牛奶"}
+                    fun.Invoke("豆浆", {"热豆浆", "凉豆浆"})
+                    fun.Invoke("矿泉水", {"矿泉水"})
+                    fun.Invoke("牛奶", {"热牛奶", "凉牛奶"})
                 End Sub
 
                 Public Function Init()
-                    Dim menuTable As Table(Of Entity.MenuItem)
                     menuTable = Table(Of Entity.MenuItem).Create("menu_table")
-                    Dim menu As New Entity.MenuItem
-                    Dim tag1 As New Entity.Tag()
-
-                    menu.Price = New Decimal(5)
-                    menu.Name = "小米粥"
-                    ' menu.Tags = 
-
+                    PreGenerate()
                 End Function
 
                 Public Function Create()
