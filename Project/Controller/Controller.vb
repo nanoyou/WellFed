0e63a2cac2abc40a0dbf536fd4ef3636622f8e96
AkagawaTsurunaki <965145619@qq.com>
2022-10-12 09:24:03 +0800

    feat: 登录登出功能

--- Project/Controller/Controller.vb
+++ Project/Controller/Controller.vb
@@ -3,6 +3,7 @@ Namespace AkagawaTsurunaki
     Namespace WellFed
         Namespace Controller
             Public Class Controller
+                Private Shared Property loginedMember As Entity.Member
 
                 Public Shared ReadOnly Property Instance = New Controller()
 
@@ -11,6 +12,14 @@ Namespace AkagawaTsurunaki
                     Service.TagService.Instance.Init()
                     Service.MenuService.Instance.Init()
                 End Function
+
+
+                Public Function Login(ByVal telephone As String, ByVal rawPassword As String)
+                    ' md5 加密如果可能
+                    Dim md5Password = rawPassword
+                    Service.MemberService.Instance.Login(telephone, md5Password)
+                End Function
+
             End Class
         End Namespace
     End Namespace
