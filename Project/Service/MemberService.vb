5f785b0333f0521d266fbd2ac82d594446a02b7a
AkagawaTsurunaki <965145619@qq.com>
2022-10-12 09:24:46 +0800

    feat: 增加临时登录功能

--- Project/Service/MemberService.vb
+++ Project/Service/MemberService.vb
@@ -8,6 +8,8 @@ Namespace AkagawaTsurunaki
             Public Class MemberService : Implements Interfaces.PreGenerator
                 Public Shared ReadOnly Property Instance As MemberService = New MemberService()
 
+                Public Shared Property MemberLogined As Entity.Member
+
                 Public Sub Init()
                     Mapper.MemberMapper.Instance.Init()
                     PreGenerate()
@@ -16,13 +18,35 @@ Namespace AkagawaTsurunaki
                 Private Sub New()
 
                 End Sub
+                ''' <summary>
+                ''' 用户登陆
+                ''' </summary>
+                ''' <param name="telephone">会员手机号码</param>
+                ''' <param name="md5Password">会员密码</param>
+                ''' <returns>是否登陆成功</returns>
+                Public Function Login(ByVal telephone As String, ByVal md5Password As String) As Boolean
+                    Dim member = Mapper.MemberMapper.Instance.FindMemberByTelephone(telephone)
+                    If member Is Nothing Then
+                        Return False
+                    End If
+                    If member.PassWord = md5Password Then
+                        MemberLogined = member
+                        Return True
+                    End If
+                    Return False
+                End Function
+
+                Public Function Logout() As Boolean
+                    MemberLogined = Nothing
+                    Return True
+                End Function
 
                 Private Sub PreGenerate() Implements PreGenerator.PreGenerate
                     For counter = 1 To 20
                         Dim m = New Entity.Member()
-                        m.Telephone = CStr(Rnd() * 10000000000)
+                        m.Telephone = CStr(Rnd() * 10000) + CStr(Rnd() * 100000)
                         m.Balance = New Decimal(Rnd() * 100)
-                        m.PassWord = "psw" + CStr(Rnd() * 1000000000000)
+                        m.PassWord = CStr(Rnd() * 1000000)
                         Mapper.MemberMapper.Instance.AddMember(m)
                     Next
                     Mapper.MemberMapper.Instance.Print()
