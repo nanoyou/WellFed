082a53679cc15e607548b98512a9cb5153596f8f
AkagawaTsurunaki <965145619@qq.com>
2022-10-13 14:03:45 +0800

    refactor: 进一步对其抽象

--- Project/Mapper/TagMapper.vb
+++ Project/Mapper/TagMapper.vb
@@ -3,40 +3,15 @@
 Namespace AkagawaTsurunaki
     Namespace WellFed
         Namespace Mapper
-            Public Class TagMapper
+            Public Class TagMapper : Inherits Mapper.Mapper(Of Entity.Tag)
                 Public Shared ReadOnly Property Instance As TagMapper = New TagMapper()
-                Private Shared Property tagTable As Table(Of Entity.Tag)
-                ''' <summary>
-                ''' 初始化TagMapper, 这是必要的
-                ''' </summary>
-                Public Sub Init()
-                    tagTable = Table(Of Entity.Tag).Create("tag_table")
-                End Sub
-                ''' <summary>
-                ''' 添加一个Tag到数据库
-                ''' </summary>
-                ''' <param name="tag"></param>
-                Public Sub AddTag(ByRef tag As Entity.Tag)
-                    tagTable.Insert(tag)
-                End Sub
-                Public Function FindTagById(ByVal id As UInteger) As Entity.Tag
-                    Return tagTable.SelectRecord(id)
-                End Function
 
                 ''' <summary>
-                ''' 获取所有的标签
+                ''' 返回根标签
                 ''' </summary>
                 ''' <returns></returns>
-                Public Function FindTags() As List(Of Entity.Tag)
-                    Return tagTable.RecordList
-                End Function
-
-                Public Sub Print()
-                    tagTable.Print()
-                End Sub
-
                 Public Function SelectRootTag() As Entity.Tag
-                    For Each tag In FindTags()
+                    For Each tag In SelectAll()
                         If tag.Id = 1 Then
                             Return tag
                         End If
@@ -50,20 +25,22 @@ Namespace AkagawaTsurunaki
                 ''' <param name="tagId"></param>
                 ''' <returns></returns>
                 Public Function SelectParentTag(ByVal tagId As UInteger) As Entity.Tag
-                    Dim tag = FindTagById(tagId)
+
+                    Dim tag = SelectRecord(tagId)
                     If tag Is Nothing Then
                         Return Nothing
                     End If
-                    Return FindTagById(tag.ParentTagId)
+                    Return SelectRecord(tag.ParentTagId)
                 End Function
+
                 ''' <summary>
-                ''' 返回一个列表, 包括了指定父标签id的所有标签
+                ''' 返回一个列表, 包括了含有父标签id的所有标签
                 ''' </summary>
                 ''' <param name="tagId"></param>
                 ''' <returns></returns>
                 Public Function SelectTagsByParentId(ByVal tagId As UInteger) As List(Of Entity.Tag)
                     Dim result As New List(Of Entity.Tag)
-                    For Each tag In FindTags()
+                    For Each tag In SelectAll()
                         If tag.ParentTagId = tagId Then
                             result.Add(tag)
                         End If
@@ -71,18 +48,13 @@ Namespace AkagawaTsurunaki
                     Return result
                 End Function
 
-                Public Function IsLeafTag(tagId As UInteger) As Boolean
-                    For Each tag In FindTags()
-                        If tag.ParentTagId = tagId Then
-                            Return False
-                        End If
-                    Next
-                    Return True
-                End Function
-
+                ''' <summary>
+                ''' 返回一个列表, 包括了根标签下的最叶标签
+                ''' </summary>
+                ''' <returns></returns>
                 Public Function SelectLeafTags() As List(Of Entity.Tag)
                     Dim result As New List(Of Entity.Tag)
-                    For Each tag In FindTags()
+                    For Each tag In SelectAll()
                         If IsLeafTag(tag.Id) Then
                             result.Add(tag)
                         End If
@@ -90,8 +62,21 @@ Namespace AkagawaTsurunaki
                     Return result
                 End Function
 
-            End Class
+                ''' <summary>
+                ''' 判断标签是否为最叶标签
+                ''' </summary>
+                ''' <param name="tagId"></param>
+                ''' <returns></returns>
+                Public Function IsLeafTag(tagId As UInteger) As Boolean
+                    For Each tag In SelectAll()
+                        If tag.ParentTagId = tagId Then
+                            Return False
+                        End If
+                    Next
+                    Return True
+                End Function
 
+            End Class
         End Namespace
     End Namespace
 End Namespace
