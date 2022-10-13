823b955ae480aaa6f0edae40bf294ef38ce8f7d3
AkagawaTsurunaki <965145619@qq.com>
2022-10-13 13:40:17 +0800

    feat: 抽象出一个Mapper

---
+++ Project/Mapper/Mapper.vb
@@ -0,0 +1,69 @@
+﻿Imports WellFed.AkagawaTsurunaki.WellFed
+
+Namespace AkagawaTsurunaki
+    Namespace WellFed
+        Namespace Mapper
+            Public Class Mapper(Of T As Entity.HasPrimaryKey)
+
+                Private Property table As WellDataBase.Table(Of T)
+
+                ''' <summary>
+                ''' 保护的构造器
+                ''' </summary>
+                Protected Sub New()
+
+                End Sub
+
+                ''' <summary>
+                ''' 初始化MemberMapper, 这是必要的
+                ''' </summary>
+                Public Function Init(tableName As String) As Boolean
+                    table = WellDataBase.Table(Of T).Create(tableName)
+                    Return True
+                End Function
+
+                ''' <summary>
+                ''' 返回所有的记录
+                ''' </summary>
+                ''' <returns></returns>
+                Public Function SelectAll() As List(Of T)
+                    Return table.RecordList
+                End Function
+
+                ''' <summary>
+                ''' 插入一条记录
+                ''' </summary>
+                ''' <param name="record"></param>
+                ''' <returns></returns>
+                Public Function Insert(ByRef record As T)
+                    Return table.Insert(record)
+                End Function
+
+                ''' <summary>
+                ''' 插入若干条记录
+                ''' </summary>
+                ''' <param name="records"></param>
+                ''' <returns></returns>
+                Public Function Insert(ByRef records As List(Of T))
+                    For Each record In records
+                        If Not Insert(record) Then
+                            Return False
+                        End If
+                    Next
+                    Return True
+                End Function
+
+                ''' <summary>
+                ''' 用于测试
+                ''' </summary>
+                Public Sub Print()
+                    table.Print()
+                End Sub
+
+
+            End Class
+
+        End Namespace
+    End Namespace
+End Namespace
+
