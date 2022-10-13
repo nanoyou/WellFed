3152e41a3b682e9e11f35d1b7db5e1c855cddc14
AkagawaTsurunaki <965145619@qq.com>
2022-10-13 13:52:16 +0800

    refactor: 进一步对其抽象

--- Project/Mapper/MenuItemMapper.vb
+++ Project/Mapper/MenuItemMapper.vb
@@ -4,28 +4,18 @@ Imports WellFed.AkagawaTsurunaki.WellFed.WellDataBase
 Namespace AkagawaTsurunaki
     Namespace WellFed
         Namespace Mapper
-            Public Class MenuItemMapper
+            Public Class MenuItemMapper : Inherits Mapper.Mapper(Of Entity.MenuItem)
 
-                Public Shared ReadOnly Property Instance As MenuItemMapper = New MenuItemMapper()
-                Private Shared Property menuItemTable As Table(Of Entity.MenuItem)
-                Public Sub InsertMember(ByRef menuItem As Entity.MenuItem)
-                    menuItemTable.Insert(menuItem)
-                End Sub
-                Public Sub Init()
-                    menuItemTable = Table(Of Entity.MenuItem).Create("menu_item_table")
-                End Sub
+                Public Shared ReadOnly Property INSTANCE As MenuItemMapper = New MenuItemMapper()
 
-                Public Sub Print()
-                    menuItemTable.Print()
-                End Sub
-
-                Public Function SelectAll() As List(Of Entity.MenuItem)
-                    Return menuItemTable.RecordList
-                End Function
-
+                ''' <summary>
+                ''' 根据Tag搜索相关的菜单项
+                ''' </summary>
+                ''' <param name="tagId"></param>
+                ''' <returns></returns>
                 Public Function SelectMenuItemByTag(tagId As UInteger) As List(Of Entity.MenuItem)
                     Dim result = New List(Of Entity.MenuItem)
-                    For Each m In menuItemTable.RecordList
+                    For Each m In table.RecordList
                         For Each tagIdInMenuItem In m.TagIds
                             If tagId = tagIdInMenuItem Then
                                 result.Add(m)
